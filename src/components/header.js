dfa46e50b0abb0ba021ca2152e95085fbffd6feb
walterperez <waltermaniaco@gmail.com>
2019-04-25 16:51:29 +0100

    #trying to fix bug with heroku

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,47 @@
 import React from "react";
-import logo from './../logo.svg';
+import styled from "styled-components";
+//Images
+import logo from "./../logo.svg";
+import hamburgerIconImg from "./../img/hamburgerIcon.svg";
 
-export default function Header () {
-    return (
-    <header
-      className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo" />
-      </header>
-    )
+const HeaderMainDiv = styled.header`
+  position: relative;
+  top: 0;
+  z-index: 10;
+  width: 100%;
+  background-color: hsl(203, 89%, 63%);
+  min-height: 80px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const HeaderHamburger = styled.div`
+  flex: 1;
+  max-width: 40px;
+  height: auto;
+  padding: 0;
+  margin: 0;
+  z-index: 100;
+`;
+
+const HamburgerIcon = styled.img``;
+
+const HeaderLogo = styled.img`
+  height: 50px;
+  pointer-events: none;
+`;
+
+export default function Header() {
+  return (
+    <HeaderMainDiv>
+      <HeaderHamburger>
+        <HamburgerIcon src={hamburgerIconImg} alt="hamburger icon" />
+      </HeaderHamburger>
+      <HeaderLogo src={logo} className="App-logo" alt="logo" />
+    </HeaderMainDiv>
+  );
 }
