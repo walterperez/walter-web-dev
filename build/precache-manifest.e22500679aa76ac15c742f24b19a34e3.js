13d43ea813a57ef9bd04c0d0701ba7b69fe21bce
walterperez <waltermaniaco@gmail.com>
2019-04-25 10:03:46 +0100

    #git ingnore build remove

---
+++ build/precache-manifest.e22500679aa76ac15c742f24b19a34e3.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c6be162227219f6a515",
+    "url": "/static/css/main.493fd560.chunk.css"
+  },
+  {
+    "revision": "3c6be162227219f6a515",
+    "url": "/static/js/main.95790120.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "625312089603c034028e",
+    "url": "/static/css/2.4ccc1aa3.chunk.css"
+  },
+  {
+    "revision": "625312089603c034028e",
+    "url": "/static/js/2.1f48b25d.chunk.js"
+  },
+  {
+    "revision": "b848d5d9bb221592064de0f356f61676",
+    "url": "/static/media/css-3.b848d5d9.png"
+  },
+  {
+    "revision": "05ce0156ef3ee6895ebdf1e4b7cfc72f",
+    "url": "/static/media/js.05ce0156.png"
+  },
+  {
+    "revision": "55e83371dbf50a5f027b1fe31a054ad3",
+    "url": "/static/media/nodejs.55e83371.png"
+  },
+  {
+    "revision": "f297dfcd8b3e9215ee862f232d5c9d13",
+    "url": "/static/media/react.f297dfcd.png"
+  },
+  {
+    "revision": "684343b1eddc6ecde169cc3b33baf987",
+    "url": "/static/media/waveBackground2.684343b1.png"
+  },
+  {
+    "revision": "bb82ed7ba8306fbe764313c55d83b4a7",
+    "url": "/static/media/logo.bb82ed7b.svg"
+  },
+  {
+    "revision": "eeda0c1d88430400b15ad0869fd8e104",
+    "url": "/static/media/hero.eeda0c1d.svg"
+  },
+  {
+    "revision": "bebf084b449be7f60603a656f6bd58f8",
+    "url": "/static/media/internet.bebf084b.png"
+  },
+  {
+    "revision": "0e51857724cb395035311f70256c7b8f",
+    "url": "/static/media/paper-plane.0e518577.png"
+  },
+  {
+    "revision": "a4231472b7d701c6c976d1f794306b2b",
+    "url": "/static/media/chat.a4231472.png"
+  },
+  {
+    "revision": "9a2a1dd487a2acba1711e6fd0bdb92fc",
+    "url": "/static/media/paint-palette.9a2a1dd4.png"
+  },
+  {
+    "revision": "91a538154f9d949e9cb6adcd7593e1bc",
+    "url": "/static/media/wave.91a53815.png"
+  },
+  {
+    "revision": "ed23881c11d00852d8e036b926ae7d8c",
+    "url": "/static/media/cuttingguru.ed23881c.png"
+  },
+  {
+    "revision": "19e88d9c34e8992be0eb4537e53f02ed",
+    "url": "/static/media/walkerbarbershop.19e88d9c.png"
+  },
+  {
+    "revision": "c1b6caabf88dece981f8e7a443a1d0ad",
+    "url": "/static/media/walterrepo.c1b6caab.png"
+  },
+  {
+    "revision": "df5e1396eb8b5d8c5f559c3a945cd09b",
+    "url": "/static/media/wd1.df5e1396.png"
+  },
+  {
+    "revision": "998d290780eb70eb5395e8038665ce9a",
+    "url": "/static/media/kodflix.998d2907.png"
+  },
+  {
+    "revision": "66019f582fed0d1cacc8102a002efce4",
+    "url": "/static/media/redux.66019f58.png"
+  },
+  {
+    "revision": "f4be0cc0f6dae8a6151deff12c54d6bd",
+    "url": "/static/media/wordpress.f4be0cc0.png"
+  },
+  {
+    "revision": "49bb761b61f274c242ae04d7ccd6547f",
+    "url": "/static/media/sass.49bb761b.jpg"
+  },
+  {
+    "revision": "13ad1a781ba845175f059418caf73414",
+    "url": "/static/media/mongodb.13ad1a78.png"
+  },
+  {
+    "revision": "5cd4a8c8b43fa8bd9e62d3e7afb03079",
+    "url": "/static/media/github.5cd4a8c8.png"
+  },
+  {
+    "revision": "8094ac49430466fa41e7e7849acb9b53",
+    "url": "/index.html"
+  }
+];
